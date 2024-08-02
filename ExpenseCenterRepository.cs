3b06af843358e9d5570d0cc80622376b26eda802
snake9byte <snake9byte@tutamail.com>
2024-08-02 15:25:07 +0300

    add tests

---
+++ ExpenseCenterRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System.Linq;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(IExpenseCenterRepository))]
+    public class ExpenseCenterRepository : BaseRepository<ExpenseCenterEntity, long>, IExpenseCenterRepository
+    {
+        public async Task<ListQueryResult<ExpenseCenterView>> GetExpenseCentersAsync(QueryDataSource<ExpenseCenterView> queryDataSource , int languageRef)
+        {
+            var result = new ListQueryResult<ExpenseCenterView>
+            {
+                Entities = new List<ExpenseCenterView>()
+            };
+
+            var queryResult = from ec in GenericRepository.Query<ExpenseCenterEntity>()
+                              join ecl in GenericRepository.Query<ExpenseCenterLanguageEntity>()
+                              on ec.Key equals ecl.ExpenseCenterRef
+                              where ecl.LanguageRef == languageRef
+                              select new ExpenseCenterView
+                              {
+                                  Key = ec.Key,
+                                  CompanyRef = ec.CompanyRef,
+                                  IsActive = ec.IsActive,
+                                  _Title = ecl._Title,
+                                  LanguageRef = ecl.LanguageRef
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(queryDataSource);
+
+            return result;
+        }
+    }
+}
---
+++ GLNRepository.cs
@@ -0,0 +1,89 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using ICD.Framework.Extensions;
+using ICD.Base.Domain.External_Entity.HRM;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(IGLNRepository))]
+    public class GLNRepository : BaseRepository<GLNEntity, long>, IGLNRepository
+    {
+        public async Task<ListQueryResult<GLNView>> GetGLNAsync(QueryDataSource<GLNView> searchQuery, int languageRef)
+        {
+            var result = new ListQueryResult<GLNView>
+            {
+                Entities = new List<GLNView>()
+            };
+
+            var queryResult = from g in GenericRepository.Query<GLNEntity>()
+
+                              join gl in GenericRepository.Query<GLNLanguageEntity>() on g.Key equals gl.GLNRef
+
+                              join pl in GenericRepository.Query<PersonLanguageEntity>() on g.PersonRef equals pl.PersonRef
+
+                              where gl.LanguageRef == languageRef &&
+                              pl.LanguageRef == languageRef
+
+                              select new GLNView
+                              {
+                                  Key = g.Key,
+                                  PersonRef = g.PersonRef,
+                                  GLN = g.GLN,
+                                  GLNLanguageRef = gl.Key,
+                                  _Title = gl._Title,
+                                  _Address = gl._Address,
+                                  FullName = pl.FullName
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            return result;
+        }
+
+        public async Task<ListQueryResult<GLNCoView>> GetGLNsOfCompanyAsync(QueryDataSource<GLNCoView> searchQuery, int languageRef)
+        {
+            var result = new ListQueryResult<GLNCoView>
+            {
+                Entities = new List<GLNCoView>()
+            };
+
+            var queryResult = from g in GenericRepository.Query<GLNEntity>()
+
+                              join gl in GenericRepository.Query<GLNLanguageEntity>() on g.Key equals gl.GLNRef
+
+                              join c in GenericRepository.Query<CompanyEntity>() on g.PersonRef equals c.PersonRef
+
+                              join cl in GenericRepository.Query<CompanyLanguageEntity>() on c.Key equals cl.CompanyRef
+
+                              where gl.LanguageRef == languageRef &&
+                              cl.LanguageRef == languageRef
+
+                              select new GLNCoView
+                              {
+                                  Key = g.Key,
+                                  PersonRef = g.PersonRef,
+                                  GLN = g.GLN,
+                                  LanguageRef = gl.LanguageRef,
+                                  _Title = gl._Title,
+                                  _Address = gl._Address,
+                                  CompanyKey = c.Key,
+                                  Company_Title = cl._Title,
+                                  GLN_TitleGLN = gl._Title + " - " + g.GLN
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            return result;
+        }
+    }
+}
