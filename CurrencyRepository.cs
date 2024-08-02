4db6ac5f9ac7df9ca2b0e7149da06d4d19d3fb90
snake9byte <snake9byte@tutamail.com>
2024-08-02 15:24:55 +0300

    add pagination

---
+++ CurrencyRepository.cs
@@ -0,0 +1,47 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(ICurrencyRepository))]
+    public class CurrencyRepository : BaseRepository<CurrencyEntity, byte>, ICurrencyRepository
+    {
+        public async Task<ListQueryResult<CurrencyView>> GetCurrencyAsync(QueryDataSource<CurrencyView> searchQuery, int languageRef)
+        {
+            var result = new ListQueryResult<CurrencyView>
+            {
+                Entities = new List<CurrencyView>()
+            };
+
+            var queryResult = from c in GenericRepository.Query<CurrencyEntity>()
+                              join cl in GenericRepository.Query<CurrencyLanguageEntity>()
+                              on c.Key equals cl.CurrencyRef
+                              where cl.LanguageRef == languageRef
+                              select new CurrencyView
+                              {
+                                  Key = c.Key,
+                                  IsActive = c.IsActive,
+                                  Icon = c.Icon,
+                                  IsNational = c.IsNational,
+                                  CurrencyRef = cl.CurrencyRef,
+                                  LanguageRef = cl.LanguageRef,
+                                  _Title = cl._Title
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            return result;
+        }
+    }
+}
---
+++ ExpenseCenterLanguageRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System.Linq;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(IExpenseCenterLanguageRepository))]
+    public class ExpenseCenterLanguageRepository : BaseRepository<ExpenseCenterLanguageEntity, long>, IExpenseCenterLanguageRepository
+    {
+        public async Task<ListQueryResult<ExpenseCenterLanguageEntity>> GetExpenseCenterLanguagesAsync(QueryDataSource<ExpenseCenterLanguageEntity> queryDataSource, int languageRef)
+        {
+            var result = new ListQueryResult<ExpenseCenterLanguageEntity>
+            {
+                Entities = new List<ExpenseCenterLanguageEntity>()
+            };
+
+            var queryResult = from ecl in GenericRepository.Query<ExpenseCenterLanguageEntity>()
+                              where ecl.LanguageRef == languageRef
+                              select ecl;
+
+            result = await queryResult.ToListQueryResultAsync(queryDataSource);
+
+            return result;
+        }
+    }
+}
