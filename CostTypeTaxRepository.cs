c55470acac47aa27ffcef8ab838c6f5d1d273352
snake9byte <snake9byte@tutamail.com>
2024-08-02 15:24:38 +0300

    add pagination

---
+++ CostTypeTaxRepository.cs
@@ -0,0 +1,53 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(ICostTypeTaxRepository))]
+    public class CostTypeTaxRepository : BaseRepository<CostTypeTaxEntity, int>, ICostTypeTaxRepository
+    {
+        public async Task<ListQueryResult<CostTypeTaxView>> GetCostTypeTaxAsync(QueryDataSource<CostTypeTaxView> searchQuery, int languageRef, short costTypeRef)
+        {
+            var result = new ListQueryResult<CostTypeTaxView>
+            {
+                Entities = new List<CostTypeTaxView>()
+            };
+
+            var queryResult = from t in GenericRepository.Query<TaxEntity>()
+
+                              join tl in GenericRepository.Query<TaxLanguageEntity>().Where(x => x.LanguageRef == languageRef) on t.Key equals tl.TaxRef
+
+                              join ctt in GenericRepository.Query<CostTypeTaxEntity>().Where(x => x.CostTypeRef == costTypeRef) on t.Key equals ctt.TaxRef
+                              into leftCtt
+                              from lctt in leftCtt.DefaultIfEmpty()
+
+                              select new CostTypeTaxView
+                              {
+                                  Key = t.Key,
+                                  TaxPercent = t.TaxPercent,
+                                  Alias = t.Alias,
+                                  IsActive = t.IsActive,
+                                  LanguageRef = tl.LanguageRef,
+                                  _Title = tl._Title,
+                                  _Description = tl._Description,
+                                  CostTypeRef = lctt.CostTypeRef,
+                                  TaxRef = lctt.TaxRef
+                              };
+
+            result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            return result;
+        }
+    }
+}
---
+++ CountryRepository.cs
@@ -0,0 +1,47 @@
+﻿using ICD.Base.Domain.Entity;
+using ICD.Base.Domain.View;
+using ICD.Base.RepositoryContract;
+using ICD.Framework.Data.Repository;
+using ICD.Framework.DataAnnotation;
+using ICD.Framework.Model;
+using ICD.Framework.QueryDataSource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using ICD.Framework.Extensions;
+
+namespace ICD.Base.Repository
+{
+    [Dependency(typeof(ICountryRepository))]
+    public class CountryRepository : BaseRepository<CountryEntity, int>, ICountryRepository
+    {
+        public async Task<ListQueryResult<CountryView>> GetCountryAsync(QueryDataSource<CountryView> searchQuery, int languageRef)
+        {
+            var finalResult = new ListQueryResult<CountryView>
+            {
+                Entities = new List<CountryView>()
+            };
+
+            var queryResult = from c in GenericRepository.Query<CountryEntity>()
+                              join cl in GenericRepository.Query<CountryLanguageEntity>()
+                              on c.Key equals cl.CountryRef
+                              where cl.LanguageRef == languageRef
+                              select new CountryView
+                              {
+                                  Key = c.Key,
+                                  IsActive = c.IsActive,
+                                  CountryRef = cl.CountryRef,
+                                  LanguageRef = cl.LanguageRef,
+                                  _Title = cl._Title
+                              };
+
+            var result = await queryResult.ToListQueryResultAsync(searchQuery);
+
+            finalResult = result;
+
+            return finalResult;
+        }
+    }
+}
